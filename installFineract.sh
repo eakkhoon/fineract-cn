2dcef7f88ccdcc8d197cef61debad3ff283defdc
Lex Pattison <161617+lexpattison@users.noreply.github.com>
2018-08-02 14:41:02 -0600

    Update installFineract.sh
    typo

--- installFineract.sh
+++ installFineract.sh
@@ -4,7 +4,7 @@ ARG1=${1:-fineract}
 ARG2=${2:-apache}
 
 mkdir $ARG1
-cd $lexpattison
+cd $ARG1
 git clone https://github.com/lexpattison/fineract-cn
 mv ./fineract-cn/initial-setup.sh ./
 bash initial-setup.sh $ARG2
